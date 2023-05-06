b40058741060067d35401e955fc1bb617d83d06e
Jonas <jonasexplore@gmail.com>
2023-05-05 21:03:40 -0300

    test: initial customer tests

---
+++ src/entities/customer.spec.ts
@@ -0,0 +1,24 @@
+import Address from "./address";
+import Customer from "./customer";
+
+describe("Customer", () => {
+  const address = new Address(
+    "1",
+    "123 Main St",
+    "Anytown",
+    "Anystate",
+    "12345"
+  );
+
+  it("should throw error when id is empty", () => {
+    expect(() => {
+      const customer = new Customer("", "John Doe", address);
+    }).toThrowError("Id is required");
+  });
+
+  it("should throw error when name is empty", () => {
+    expect(() => {
+      const customer = new Customer("1", "", address);
+    }).toThrowError("Name is required");
+  });
+});
