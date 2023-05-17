753ede4331f7679f9faa53112d29a94a5008abb2
Jonas <jonasexplore@gmail.com>
2023-05-17 09:37:57 -0300

    chore(domain): create common

---
+++ src/domain/common/event/event-dispatcher.spec.ts
@@ -0,0 +1,52 @@
+import SendEmailWhenProductIsCreated from "../../product/events/handlers/send-email-when-product-is-created.handler";
+import ProductCreatedEvent from "../../product/events/product-created.event";
+import EventDispatcher from "./event-dispatcher";
+
+describe("EventDispatcher", () => {
+  it("should register an event handler", () => {
+    const dispatcher = new EventDispatcher();
+    const handler = new SendEmailWhenProductIsCreated();
+    dispatcher.register("ProductCreatedEvent", handler);
+    expect(dispatcher.getHandlers.get("ProductCreatedEvent")).toEqual([
+      handler,
+    ]);
+  });
+
+  it("should unregister an event handler", () => {
+    const dispatcher = new EventDispatcher();
+    const handler = new SendEmailWhenProductIsCreated();
+    dispatcher.register("ProductCreatedEvent", handler);
+    expect(dispatcher.getHandlers.get("ProductCreatedEvent")).toEqual([
+      handler,
+    ]);
+    dispatcher.unregister("ProductCreatedEvent", handler);
+    expect(dispatcher.getHandlers.get("ProductCreatedEvent")).toEqual([]);
+  });
+
+  it("should unregister all event handlers", () => {
+    const dispatcher = new EventDispatcher();
+    const handler = new SendEmailWhenProductIsCreated();
+    dispatcher.register("ProductCreatedEvent", handler);
+    expect(dispatcher.getHandlers.get("ProductCreatedEvent")).toEqual([
+      handler,
+    ]);
+    dispatcher.unregisterAll();
+    expect(dispatcher.getHandlers.get("ProductCreatedEvent")).toEqual(
+      undefined
+    );
+  });
+
+  it("should notify all event handlers", () => {
+    const dispatcher = new EventDispatcher();
+    const handler = new SendEmailWhenProductIsCreated();
+    dispatcher.register("ProductCreatedEvent", handler);
+    const productCreatedEvent = new ProductCreatedEvent({
+      name: "ProductCreatedEvent",
+      description: "Product created",
+      price: 100,
+    });
+    const spy = jest.spyOn(handler, "handle");
+    dispatcher.notify(productCreatedEvent);
+    expect(spy).toBeCalledTimes(1);
+  });
+});
